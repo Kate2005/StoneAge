b1bc8f6ed2d0e2626ad609dc0af89fac4a47549d
Kate <86675857+Kate2005@users.noreply.github.com>
2021-08-12 18:58:14 +0300

    init comit

---
+++ StoneAgeGroup1-main/Stone Age Group1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Property")]
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float playerJumpForce;
+
+    [SerializeField] private GameObject animObject;
+
+    private SpriteRenderer spriteRenderer;
+
+    private float currentPlayerSpeed;
+    private Rigidbody2D rb;
+    private bool groundCheck;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = animObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //ДЗ добавить enum
+        rb.velocity = new Vector2(currentPlayerSpeed*Time.deltaTime, rb.velocity.y);  
+        //rb.AddForce(new Vector2(currentPlayerSpeed, 0f), ForceMode2D.Force);
+    }
+
+    public void RightMove()
+    {
+        currentPlayerSpeed = playerSpeed;
+        spriteRenderer.flipX = false; 
+    }
+    public void LeftMove()
+    {
+        currentPlayerSpeed = -playerSpeed;
+        spriteRenderer.flipX = true;    
+    }
+
+ 
+    public void StopMove()
+    {
+        currentPlayerSpeed = 0f;
+    }
+    public void Jump()
+    {
+        if (groundCheck)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
+            groundCheck = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        groundCheck = true;
+    }
+
+}
+
+
+
+
+
+
+//if (transform.localScale.x > 0)
+//{
+//    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.y);
+//}
